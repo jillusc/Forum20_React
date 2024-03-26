9ac2eb0c5cc9eb2ce630dd5ca0daf7b4968c00d2
jillusc <jillus@hotmail.com>
2024-03-26 16:05:49 +0000

    Set up routing for home, log in and sign up pages with react-router-dom and NavLink.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,32 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-      <Navbar.Brand>
-          <img src={logo} alt="logo" height="65" />
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="65" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto mt-4 text-left">
-            <Nav.Link>
+            <NavLink exact className={styles.NavLink}
+              activeClassName={styles.Active} to="/" >
               <i className="fa-solid fa-house"></i>Home
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink className={styles.NavLink}
+              activeClassName={styles.Active} to="/login">
               <i className="fa-solid fa-right-to-bracket"></i>Log in
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink className={styles.NavLink}
+              activeClassName={styles.Active} to="/signup">
               <i className="fa-solid fa-user-plus"></i>Sign up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
