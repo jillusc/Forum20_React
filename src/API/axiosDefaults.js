a1cf2ddff405d9ffcb909219f57cae58079d53bf
jillusc <jillus@hotmail.com>
2024-03-27 21:26:38 +0000

    Implemented axios interceptors for automatic token refresh on 401 errors.

--- src/API/axiosDefaults.js
+++ src/API/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://forum20-a5050d8397b8.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
