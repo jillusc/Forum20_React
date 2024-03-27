aefe71b2b4fb13ba870733fc7b4443ae2fb49f37
jillusc <jillus@hotmail.com>
2024-03-27 22:44:29 +0000

    Addressed sudden 'Module not found' error in CurrentUserContext.js!

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
-import { axiosReq, axiosRes } from "../api/axiosDefaults";
+import { axiosReq, axiosRes } from "../API/axiosDefaults";
 import { useHistory } from "react-router";
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
