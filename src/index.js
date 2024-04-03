61f14bb73e6889f10185234893a496950cd57511
jillusc <jillus@hotmail.com>
2024-04-03 09:38:53 +0000

    Created ProfileDataContext file to handle state for PopularProfiles component via context.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from "react-router-dom";
 import { CurrentUserProvider } from './contexts/CurrentUserContext';
-
+import { ProfileDataProvider } from './contexts/ProfileDataContext';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <CurrentUserProvider>
-      <App />
+        <ProfileDataProvider>
+          <App />
+        </ProfileDataProvider>
       </CurrentUserProvider>
     </Router>
   </React.StrictMode>,
