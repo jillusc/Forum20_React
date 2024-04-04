7bd0df7fad9fc456a9169a4867916409604647c5
jillusc <jillus@hotmail.com>
2024-04-04 10:37:45 +0000

    Created NotFound component for 401 pages and styled.

---
+++ src/components/NotFound.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import NoResults from "../assets/no-results-icon.png";
+import styles from "../styles/NotFound.module.css";
+import Asset from "./Asset";
+
+const NotFound = () => {
+  return (
+    <div className={styles.NotFound}>
+      <Asset
+        src={NoResults}
+        message={`The page you're looking for doesn't exist`} />
+        <Link to="/" className={styles.HomeLink}>Back to Forum20 homepage</Link>
+    </div>
+  );
+};
+
+export default NotFound;
