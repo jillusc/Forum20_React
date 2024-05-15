f5892d4ecff4969f0aeb696aa62916e0dfd2a286
jillusc <jillus@hotmail.com>
2024-05-15 17:34:47 +0000

    Added a Likes tab to complete the UserActivity page.

--- src/pages/user_activity/UserActivity.js
+++ src/pages/user_activity/UserActivity.js
@@ -20,24 +20,29 @@ import { fetchMoreData } from "../../utils/utils";
 
 const UserActivity = ({ mobile }) => {
     const currentUser = useCurrentUser();
+    const [userLikes, setUserLikes] = useState({ results: [], loading: true, error: null });
     const [userComments, setUserComments] = useState({ results: [], loading: true, error: null });
     const [userBookmarks, setUserBookmarks] = useState({ results: [], loading: true, error: null });
     const [, setErrors] = useState({});
-    const [activeTab, setActiveTab] = useState("comments");
+    const [activeTab, setActiveTab] = useState("likes");
 
     useEffect(() => {
         const fetchUserActivity = async () => {
             if (currentUser?.profile_id) {
                 try {
-                    const { data } = await axiosReq.get(`/comments/?user_id=${currentUser.profile_id}&user_comments=true`);
-
+                    const { data: likesData } = await axiosReq.get(`/likes/?user_id=${currentUser.profile_id}`);
+                    setUserLikes({
+                        results: likesData.results,
+                        loading: false,
+                        error: null
+                    });
+                    const { data: commentsData } = await axiosReq.get(`/comments/?user_id=${currentUser.profile_id}&user_comments=true`);
                     setUserComments({
-                        results: data.results,
+                        results: commentsData.results,
                         loading: false,
                         error: null
                     });
                     const { data: bookmarksData } = await axiosReq.get(`/bookmarks/?user_id=${currentUser.profile_id}`);
-
                     setUserBookmarks({
                         results: bookmarksData.results,
                         loading: false,
@@ -61,17 +66,64 @@ const UserActivity = ({ mobile }) => {
                 <Col className="col-12 col-md-10 col-lg-8 mx-auto">
                     <div className={`${appStyles.Content} mb-4`}>
                         <h5>My recent activity</h5>
-                        {userComments.loading || userBookmarks.loading ? (
+                        {userLikes.loading || userComments.loading || userBookmarks.loading ? (
                             <Asset spinner aria-label="Loading content…" />
                         ) : (
                             <Tabs activeKey={activeTab} onSelect={handleTabChange} className={styles.tabPosition}>
-                                <Tab eventKey="comments" aria-labelledby="comments-tab" title={
-                                    <>
-                                        <div className={`${styles.TabLink} ${activeTab === "comments" ? styles.TabLink_active : ""}`}>
-                                            <i className="fa-solid fa-comment-dots" aria-hidden="true" />Comments
+                                <Tab
+                                    eventKey="likes"
+                                    aria-labelledby="likes-tab"
+                                    title={
+                                        <div className={`${styles.TabLink} ${activeTab === "likes" ? styles.TabLink_active : ""}`}>
+                                            <i className="fa-solid fa-heart" aria-hidden="true" /> Likes
                                         </div>
-                                    </>
-                                }>
+                                    }
+                                >
+                                    {userLikes.results.length === 0 ? (
+                                        <div>
+                                            <p className={styles.NoContent}>No liked posts to show.</p>
+                                        </div>
+                                    ) : (
+                                        <InfiniteScroll
+                                            dataLength={userLikes.results.length}
+                                            next={fetchMoreData}
+                                            hasMore={false}
+                                            loader={<Asset spinner aria-label="Loading content..." />}
+                                        >
+                                            {userLikes.results.map(like => (
+                                                <div key={like.id}>
+                                                    <div className="d-flex justify-content-between align-items-end w-100">
+                                                        <h6 className={styles.Header}>
+                                                            Liked post:
+                                                        </h6>
+                                                        <span className={styles.Date}>{like.created_at}</span>
+                                                    </div>
+                                                    <div className="d-flex w-100 align-items-center mb-3">
+                                                        <Link to={`/profiles/${currentUser.profile_id}`} className="mr-2">
+                                                            <Avatar src={currentUser.profile_image} />
+                                                        </Link>
+                                                        <h6 className={styles.PostTitle}>
+                                                            <Link to={`/posts/${like.post}`}>
+                                                                <span>{like.post_title}</span>
+                                                            </Link>
+                                                        </h6>
+                                                    </div>
+                                                    <hr className={styles.notHidden} />
+                                                </div>
+                                            ))}
+                                        </InfiniteScroll>
+                                    )}
+                                </Tab>
+
+                                <Tab
+                                    eventKey="comments"
+                                    aria-labelledby="comments-tab"
+                                    title={
+                                        <div className={`${styles.TabLink} ${activeTab === "comments" ? styles.TabLink_active : ""}`}>
+                                            <i className="fa-solid fa-comment-dots" aria-hidden="true" /> Comments
+                                        </div>
+                                    }
+                                >
                                     {userComments.results.length === 0 ? (
                                         <div>
                                             <p className={styles.NoContent}>No comments to show.</p>
@@ -85,7 +137,7 @@ const UserActivity = ({ mobile }) => {
                                         >
                                             {userComments.results.map(comment => (
                                                 <div key={comment.id}>
-                                                    <h6 className={styles.CommentsHeader}>
+                                                    <h6 className={styles.Header}>
                                                         On post:{" "}
                                                         <Link to={`/posts/${comment.post}`}>
                                                             <span>{comment.post_title}</span>
@@ -110,13 +162,15 @@ const UserActivity = ({ mobile }) => {
                                     )}
                                 </Tab>
 
-                                <Tab eventKey="bookmarks" aria-labelledby="bookmarks-tab" title={
-                                    <>
+                                <Tab
+                                    eventKey="bookmarks"
+                                    aria-labelledby="bookmarks-tab"
+                                    title={
                                         <div className={`${styles.TabLink} ${activeTab === "bookmarks" ? styles.TabLink_active : ""}`}>
-                                            <i className="fa-solid fa-bookmark" aria-hidden="true" />Bookmarks
+                                            <i className="fa-solid fa-bookmark" aria-hidden="true" /> Bookmarks
                                         </div>
-                                    </>
-                                }>
+                                    }
+                                >
                                     {userBookmarks.results.length === 0 ? (
                                         <div>
                                             <p className={styles.NoContent}>No bookmarked posts to show.</p>
@@ -130,23 +184,17 @@ const UserActivity = ({ mobile }) => {
                                         >
                                             {userBookmarks.results.map(bookmark => (
                                                 <div key={bookmark.id}>
-                                                    <div className={styles.Bookmark_MediaBody}>
-                                                        <div>
-                                                            <Link to={`/profiles/${currentUser.profile_id}`}>
-                                                                <Avatar src={currentUser.profile_image} />
-                                                            </Link>
-                                                        </div>
-                                                        <div>
-                                                            <h6 className={styles.BookmarksHeader}>
-                                                                Bookmarked post:
-                                                            </h6>
-                                                        </div>
-                                                        <div>
-                                                            <span className={styles.Date}>{bookmark.created_at}</span>
-                                                        </div>
+                                                    <div className="d-flex justify-content-between align-items-end w-100">
+                                                        <h6 className={styles.Header}>
+                                                            Bookmarked post:
+                                                        </h6>
+                                                        <span className={styles.Date}>{bookmark.created_at}</span>
                                                     </div>
-                                                    <div className="ml-2">
-                                                        <h6>
+                                                    <div className="d-flex w-100 align-items-center mb-3">
+                                                        <Link to={`/profiles/${currentUser.profile_id}`} className="mr-2">
+                                                            <Avatar src={currentUser.profile_image} />
+                                                        </Link>
+                                                        <h6 className={styles.PostTitle}>
                                                             <Link to={`/posts/${bookmark.post}`}>
                                                                 <span>{bookmark.post_title}</span>
                                                             </Link>
@@ -159,12 +207,11 @@ const UserActivity = ({ mobile }) => {
                                     )}
                                 </Tab>
                             </Tabs>
-                        )
-                        }
-                    </div >
+                        )}
+                    </div>
                 </Col>
             </Row>
-        </Container >
+        </Container>
     );
 };
 
